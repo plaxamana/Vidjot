6370c88773618df02b38c849f634b1e2e9932083
Philip Laxamana <philip.laxamana@hotmail.com>
2018-03-16 14:17:21 -0400

    added handlebars middleware

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 // Index route
 app.get('/', (req, res) =>{
-    res.send('index');
+    res.render('index');
 });
 
 // About route
